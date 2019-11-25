ede7a2360843395d3979857199ae16d9ec26aeed
valeriojr <41243914+valeriojr@users.noreply.github.com>
2019-11-25 11:01:11 -0300

    Planetas

--- opengl.c
+++ opengl.c
@@ -1,8 +1,21 @@
 #include <GL/glut.h>
+#include <GL/glu.h>
 #include <stdio.h>
 #include <math.h>
 
 #define min(a, b) ((a) < (b) ? a : b)
+#define max(a, b) ((a) > (b) ? a : b)
+
+enum {
+	X,
+	Y,
+	Z
+};
+
+enum {
+	Orthographic,
+	Perspective
+};
 
 int WINDOW_WIDTH = 640;
 int WINDOW_HEIGHT = 480;
@@ -12,13 +25,10 @@ char WINDOW_NAME[] = "OpenGL Application";
 float dt = 0.0f;
 float t = 0.0f;
 float ref_t = 0.0f;
-
-enum {
-	X,
-	Y,
-	Z
-};
-
+int projectionMode = Perspective;
+float cam_position[3] = {0.0f, 0.0f, 0.0f};
+float cam_rotation[3];
+float fovy = 60.0;
 
 /* GLUT callbacks */
 void init(int* argc, char** argv, int window_width, int window_height, int display_mode, const char* window_name);
@@ -28,15 +38,72 @@ void motion(int w, int h);
 void keyboard(unsigned char key, int x, int y);
 void display();
 
-void draw_line(float x0, float y0, float x1, float y1);
+void draw_line(float x0, float y0, float z0, float x1, float y1, float z1);
 void draw_rectangle(float x, float y, float w, float h, float angle);
 void draw_circle(float x, float y, float radius);
+
+void planet(float x, float y, float z, float distance, float size, float rotationSpeed){
+	//"Translation"
+	glRotatef(rotationSpeed * t, x, y, z);
+	glTranslatef(0.0f, 0.0f, distance);
+
+	//Local rotation
+	glRotatef(t, 0.0f, 1.0f, 0.0f);
+	
+	glutSolidSphere(size, 20, 20);
+}
+
+
+void planet1(){
+	glColor3f(1.0f, 0.0f, 0.0f);
+	planet(0.0f, 1.0f, 0.0f, 100.0f, 15.0f, -10.0f);
+}
+
+void planet2(){
+	glColor3f(0.0f, 0.0f, 1.0f);
+	planet(0.0f, 1.0f, 0.0f, 200.0f, 10.0f, -10.0f);
+
+	glPushMatrix();
+	glColor3f(0.5f, 0.5f, 0.5f);
+	planet(0.0f, 1.0f, 0.0f, 20.0f, 2.0f, 10.0f);
+	glPopMatrix();
+	
+	glPushMatrix();
+	glColor3f(0.25f, 0.25f, 0.25f);
+	planet(1.0f, 1.0f, 0.0f, 50.0f, 3.0f, -100.0f);
+	glPopMatrix();
+}
+
+void sun() {
+	glColor3f(1.0f, 1.0f, 0.0f);
+	planet(0.0f, 1.0f, 0.0f, 0.0f, 30.0f, 10.0f);
+
+	glPushMatrix();
+	planet1();
+	glPopMatrix();
+
+	glPushMatrix();
+	planet2();
+	glPopMatrix();
+}
+
+void solarSystem(){
+	sun();
+}
+
+
+
+
 
 void init(int* argc, char** argv, int window_width, int window_height, int display_mode, const char* window_name){
 	glutInit(argc, argv);
 	glutInitDisplayMode(display_mode);
 	glutInitWindowSize(window_width, window_height);
 	glutCreateWindow(window_name);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glCullFace(GL_BACK);
+	glPolygonMode(GL_FRONT, GL_FILL);
 
 	reshape(WINDOW_WIDTH, WINDOW_HEIGHT);	
 }
@@ -48,15 +115,27 @@ void idle(void) {
     dt = (t - old_t);
     old_t = t;
 
+    gluLookAt(cam_position[X], cam_position[Y], cam_position[Z], cam_rotation[X], cam_rotation[Y], cam_rotation[Z], 0.0f, 1.0f, 0.0f);
+
     glutPostRedisplay();
 }
 
 void reshape(int w, int h){
+	printf("reshaping\n");
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	
 	glLoadIdentity();
-	glOrtho(-1.0f, 4.0f, -1.0f, 3.0f, -1.0f, 1.0f);
+
+	float aspect = 1.0f * h / w;
+
+	switch(projectionMode){
+		case Orthographic:
+			glOrtho(-WINDOW_WIDTH / 2.0f, WINDOW_WIDTH / 2.0f, (WINDOW_WIDTH * aspect) / 2.0f, (-WINDOW_WIDTH * aspect) / 2.0f, -1000.0f, 1000.0f);
+			break;
+		case Perspective:
+			gluPerspective(fovy, aspect, 0.001, 1000.0f);
+	}
 }
 
 void motion(int x, int y){
@@ -66,69 +145,23 @@ void keyboard(unsigned char key, int x, int y){
 	if(key == 'r'){
 		ref_t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
 	}
+	if(key == 'c'){
+		projectionMode = !projectionMode;
+		reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+	}
 }
 
 void display(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-	glColor3f(0.8f, 0.8f, 0.8f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	int i;
-	for(i = 0;i < 5;i++){
-		draw_line(-1.0f, 1.0f * i, 4.0f, 1.0f * i);
-		draw_line(1.0f * i, -1.0f, 1.0f * i, 3.0f);
-	}
-	glColor3f(0.0f, 0.0f, 0.0f);
-	draw_line(0.0f, -10.0f, 0.0f, 10.0f);
-	draw_line(-10.0f, 0.0f, 10.0f, 0.0f);
+	draw_line(0.0f, 1000.0f, 0.0f, 0.0f, -1000.0f, 0.0f);
 
-
-	void draw_triangle(float sx, float sy){
-		glPushMatrix();
-		glScalef(sx, sy, 1.0f);
-
-		glBegin(GL_TRIANGLES);
-			glVertex2f(0.0f, 0.0f);
-			glVertex2f(1.0f, 2.0f);
-			glVertex2f(1.0f, 0.0f);
-		glEnd();
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glBegin(GL_LINES);
-			glVertex2f(0.0f, 0.0f);
-			glVertex2f(1.0f, 2.0f);
-			glVertex2f(1.0f, 2.0f);
-			glVertex2f(1.0f, 0.0f);
-			glVertex2f(1.0f, 0.0f);			
-			glVertex2f(0.0f, 0.0f);
-		glEnd();
-		glPopMatrix();
-
-		float radius = 0.1f;
-
-		glColor3f(0.0f, 0.0f, 0.0f);
-		draw_circle(1.0f + sx * 0.0f, sy * 0.0f, radius);
-		draw_circle(1.0f + sx * 1.0f, sy * 2.0f, radius);
-		draw_circle(1.0f + sx * 1.0f, sy * 0.0f, radius);
-		radius = 0.075f;
-		glColor3f(1.0f, 1.0f, 1.0f);
-		draw_circle(1.0f + sx * 0.0f, sy * 0.0f, radius);
-		draw_circle(1.0f + sx * 1.0f, sy * 2.0f, radius);
-		draw_circle(1.0f + sx * 1.0f, sy * 0.0f, radius);
-	}
-
-	glTranslatef(1.0f, 0.0f, 0.0f);
-	
-	glColor3f(0.3f, 0.3f, 0.3f);
- 	draw_triangle(1.0f, 1.0f);
-	
-	
-	glColor3f(0.5f, 0.5f, 0.5f);
-	draw_triangle(min(1.0f + t, 2.0f), 1.0f);
-
+	//planet(0.0f, 0.0f, 0.0f, 100.0f, 50.0f);
+	solarSystem();
  	glutSwapBuffers();
 }
 
@@ -148,10 +181,10 @@ void draw_circle(float x, float y, float radius){
 	glPopMatrix();
 }
 
-void draw_line(float x0, float y0, float x1, float y1){
+void draw_line(float x0, float y0, float z0, float x1, float y1, float z1){
 	glBegin(GL_LINES);
-		glVertex2f(x0, y0);
-		glVertex2f(x1, y1);
+		glVertex3f(x0, y0, z0);
+		glVertex3f(x1, y1, z1);
 	glEnd();
 }
 
@@ -183,9 +216,9 @@ void draw_rectangle(float x, float y, float w, float h, float angle){
 
 
 int main(int argc, char** argv){
-	init(&argc, argv, WINDOW_WIDTH, WINDOW_HEIGHT, GLUT_DOUBLE | GLUT_RGB, WINDOW_NAME);
+	init(&argc, argv, WINDOW_WIDTH, WINDOW_HEIGHT, GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH, WINDOW_NAME);
 
-	glutIdleFunc(idle);
+	glutIdleFunc(idle); 
 	glutReshapeFunc(reshape);
 	glutMotionFunc(motion);
 	glutPassiveMotionFunc(motion);
@@ -196,3 +229,4 @@ int main(int argc, char** argv){
 
 	return 0;
 }
+
